e6d2883bd5aa4c217246cd4b53d01006093516e1
kit <nickmat95@yandex.ru>
2017-09-21 00:16:24 +0300

    adding drag and drop for carts

--- src/components/Body/Columns/Column/Carts-list/Cart/Cart.jsx
+++ src/components/Body/Columns/Column/Carts-list/Cart/Cart.jsx
@@ -3,17 +3,63 @@ import { connect } from 'react-redux';
 import Text from './Text/Text.jsx';
 import EditTextButton from './Edit-text-button/Edit-text-button.jsx';
 import DeleteCartButton from './Delete-cart-button/Delete-cart-button.jsx';
+import { DragSource, DropTarget } from 'react-dnd';
+import ItemTypes from '../../../../../../item-types.js';
 import './Cart.css';
+
+const cartSource = {
+	beginDrag(props) {
+	    return {
+	    	id: props.id,
+	    	originalIndex: props.findCart(props.id).index,
+	    };
+	},
+
+	endDrag(props, monitor) {
+	    const { id: droppedId, originalIndex } = monitor.getItem();
+	    const didDrop = monitor.didDrop();
+
+	    if (!didDrop) {
+	    	props.moveCart(droppedId, originalIndex);
+	    }
+	},
+};
+
+const cartTarget = {
+	canDrop() {
+  		return false;
+	},
+
+	hover(props, monitor) {
+	    const { id: draggedId } = monitor.getItem();
+	    const { id: overId } = props;
+
+	    if (draggedId !== overId) {
+	    	const { index: overIndex } = props.findCart(overId);
+	    	props.moveCart(draggedId, overIndex);
+	    }
+	},
+};
+
+const DSource = DragSource(ItemTypes.CART, cartSource, (connect, monitor) => ({
+	connectDragSource: connect.dragSource(),
+	isDragging: monitor.isDragging(),
+}));
+
+const DTarget = DropTarget(ItemTypes.CART, cartTarget, connect => ({
+	connectDropTarget: connect.dropTarget(),
+}));
 
 class Cart extends React.Component {
 	render() {
-	    return (
+		const { isDragging, connectDragSource, connectDropTarget } = this.props;
+	    return connectDragSource(connectDropTarget(
 	    	<div className="cart">
 	    		<Text />
 	    		<EditTextButton />
 	    		<DeleteCartButton />
 	    	</div>
-	    );
+	    ));
 	}
 }
 
@@ -24,4 +70,4 @@ export default connect(
 	dispatch => ({
 
 	})
-)(Cart);
+)(DTarget(DSource(Cart)));
--- src/item-types.js
+++ src/item-types.js
@@ -1,3 +1,4 @@
 export default {
 	COLUMN: 'column',
+	CART: 'cart',
 }
